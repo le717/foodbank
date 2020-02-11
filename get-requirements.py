534313c58ef3ba5ee0d48d942de104ac6cfdefff
Caleb Ely <le717@users.noreply.github.com>
2020-02-10 21:35:25 -0500

    Rename parameter

--- get-requirements.py
+++ get-requirements.py
@@ -2,14 +2,14 @@ from sys import argv
 from toml import load
 
 
-def get_package(package: dict) -> str:
+def get_package(package_info: dict) -> str:
     # Construct the package name and exact version to install
-    package_tag = f"{package['name']}=={package['version']}"
+    package_tag = f"{package_info['name']}=={package_info['version']}"
 
     # However, if the package is a local file,
     # we need to use that instead
-    if package.setdefault("source", {}).get("url"):
-        package_tag = package["source"]["url"]
+    if package_info.setdefault("source", {}).get("url"):
+        package_tag = package_info["source"]["url"]
 
         # Trim off the app root path
         package_tag = package_tag[package_tag.find("/") + 1:]
@@ -28,11 +28,11 @@ except IndexError:
 
 # Load the lock file contents and get the respective package for each category
 poetry_lock = load(open("./poetry.lock"))
-all_packages = filter_packages(poetry_lock['package'], "main")
+all_packages = filter_packages(poetry_lock["package"], "main")
 
 # Write the dev packages if requested
 if get_dev_packages:
-    all_packages += filter_packages(poetry_lock['package'], "dev")
+    all_packages += filter_packages(poetry_lock["package"], "dev")
 
 # Open the requirements file for writing
 with open("requirements.txt", "wt") as f:
