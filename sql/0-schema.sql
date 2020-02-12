60ccb4bc514e6a1a694c8e03cba748428d931dbc
Caleb Ely <le717@users.noreply.github.com>
2020-02-12 15:14:15 -0500

    Create all foriegn key constraints

--- sql/0-schema.sql
+++ sql/0-schema.sql
@@ -13,39 +13,43 @@
 
 
 -- Dumping database structure for lighthouse
-CREATE DATABASE IF NOT EXISTS `lighthouse` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `lighthouse` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `lighthouse`;
 
--- Dumping structure for table lighthouse.authorized_pickups
-CREATE TABLE IF NOT EXISTS `authorized_pickups` (
-  `client_id` int unsigned NOT NULL,
-  `auth_client_id` int unsigned NOT NULL,
-  PRIMARY KEY (`client_id`,`auth_client_id`)
+-- Dumping structure for table lighthouse.organizations
+CREATE TABLE IF NOT EXISTS `organizations` (
+  `_id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table lighthouse.campuses
 CREATE TABLE IF NOT EXISTS `campuses` (
-  `_id` smallint NOT NULL AUTO_INCREMENT,
+  `_id` smallint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `org_id` tinyint unsigned NOT NULL,
-  PRIMARY KEY (`_id`)
+  PRIMARY KEY (`_id`),
+  CONSTRAINT `fk_campus_organization`
+    FOREIGN KEY (org_id) REFERENCES organization (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table lighthouse.clients
 CREATE TABLE IF NOT EXISTS `clients` (
-  `_id` int NOT NULL AUTO_INCREMENT COMMENT 'The client PK, used for FK constraints. Should never be exposed outside of the database.',
+  `_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'The client PK, used for FK constraints. Should never be exposed outside of the database.',
   `client_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Will be filled in using a v4 UUID provided by the app. Use this when needing to expose a client ID outside the database.',
   `first_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `alias` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `dob` datetime NOT NULL,
-  `tanf` tinyint(1) NOT NULL,
-  `at_risk` tinyint(1) NOT NULL,
-  `transitional` tinyint(1) NOT NULL,
+  `tanf` tinyint(1) unsigned NOT NULL,
+  `at_risk` tinyint(1) unsigned NOT NULL,
+  `transitional` tinyint(1) unsigned NOT NULL,
   `children` tinyint unsigned DEFAULT NULL,
   `adults` tinyint unsigned NOT NULL,
   `seniors` tinyint unsigned NOT NULL,
@@ -65,59 +69,33 @@ CREATE TABLE IF NOT EXISTS `clients` (
 
 -- Data exporting was unselected.
 
--- Dumping structure for table lighthouse.counseling_notes
-CREATE TABLE IF NOT EXISTS `counseling_notes` (
-  `_id` int NOT NULL AUTO_INCREMENT,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `comments` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `was_counseled` tinyint(1) NOT NULL,
-  `is_baptized` tinyint(1) NOT NULL,
-  `is_ccc_member` tinyint(1) NOT NULL,
-  `member_where` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `org_id` tinyint unsigned NOT NULL,
-  `client_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  PRIMARY KEY (`_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table lighthouse.organizations
-CREATE TABLE IF NOT EXISTS `organizations` (
-  `_id` tinyint NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table lighthouse.queue
-CREATE TABLE IF NOT EXISTS `queue` (
-  `_id` int NOT NULL AUTO_INCREMENT COMMENT 'Surrogate key, isn''t really useful to reference.',
-  `org_id` tinyint unsigned NOT NULL,
-  `campus_id` smallint unsigned NOT NULL,
+-- Dumping structure for table lighthouse.authorized_pickups
+CREATE TABLE IF NOT EXISTS `authorized_pickups` (
   `client_id` int unsigned NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `ticket_num` mediumint unsigned NOT NULL,
-  `is_present` tinyint(1) NOT NULL,
-  `is_counseled` tinyint(1) NOT NULL DEFAULT '0',
-  `is_completed` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`_id`),
-  UNIQUE KEY `ticket_num` (`ticket_num`)
+  `auth_client_id` int unsigned NOT NULL,
+  PRIMARY KEY (`client_id`,`auth_client_id`),
+  CONSTRAINT `fk_authorized_pickups_clients_1`
+    FOREIGN KEY (client_id) REFERENCES clients (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_authorized_pickups_clients_2`
+    FOREIGN KEY (auth_client_id) REFERENCES clients (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table lighthouse.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `_id` int NOT NULL AUTO_INCREMENT COMMENT 'The user PK, used for FK constraints. Should never be exposed outside of the database.',
+  `_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'The user PK, used for FK constraints. Should never be exposed outside of the database.',
   `user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Will be filled in using a v4 UUID provided by the app. Use this when needing to expose a user ID outside the database.',
   `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` enum('admin','member') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'member',
-  `org_id` tinyint unsigned NOT NULL DEFAULT '0',
-  `needs_password_reset` tinyint(1) DEFAULT '0' COMMENT 'Will be a truthy value whenever someone requests that their password be reset.',
+  `org_id` tinyint unsigned NOT NULL,
+  `needs_password_reset` tinyint(1) unsigned DEFAULT '0' COMMENT 'Will be a truthy value whenever someone requests that their password be reset.',
   `token_expiry_date` datetime DEFAULT NULL COMMENT 'The datetime value when the temp password token will expire.',
   `temp_password_token` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Will be filled in only when the user requests a password change.',
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -125,7 +103,69 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`_id`),
   UNIQUE KEY `user_id` (`user_id`),
   UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `temp_password_token` (`temp_password_token`)
+  UNIQUE KEY `temp_password_token` (`temp_password_token`),
+  CONSTRAINT `fk_users_organizations`
+    FOREIGN KEY (org_id) REFERENCES organizations (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table lighthouse.counseling_notes
+CREATE TABLE IF NOT EXISTS `counseling_notes` (
+  `_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comments` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `was_counseled` tinyint(1) unsigned NOT NULL,
+  `is_baptized` tinyint(1) unsigned NOT NULL,
+  `is_ccc_member` tinyint(1) unsigned NOT NULL,
+  `member_where` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `org_id` tinyint unsigned NOT NULL,
+  `client_id` int unsigned NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  PRIMARY KEY (`_id`),
+  CONSTRAINT `fk_counseling_notes_organizations`
+    FOREIGN KEY (org_id) REFERENCES organizations (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_counseling_notes_clients`
+    FOREIGN KEY (client_id) REFERENCES clients (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_counseling_notes_users`
+    FOREIGN KEY (`user_id`) REFERENCES users (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table lighthouse.queue
+CREATE TABLE IF NOT EXISTS `queue` (
+  `_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Surrogate key, isn''t really useful to reference.',
+  `org_id` tinyint unsigned NOT NULL,
+  `campus_id` smallint unsigned NOT NULL,
+  `client_id` int unsigned NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ticket_num` mediumint unsigned NOT NULL,
+  `is_present` tinyint(1) unsigned NOT NULL,
+  `is_counseled` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `is_completed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`_id`),
+  UNIQUE KEY `ticket_num` (`ticket_num`),
+  CONSTRAINT `fk_queue_organizations`
+    FOREIGN KEY (`org_id`) REFERENCES organizations (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_queue_campus`
+    FOREIGN KEY (`campus_id`) REFERENCES campuses (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_queue_client`
+    FOREIGN KEY (`client_id`) REFERENCES clients (_id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
