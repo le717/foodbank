bd97f05117e89b64c33d3ae292ab4c647ceb0118
Caleb Ely <le717@users.noreply.github.com>
2020-02-23 21:40:31 -0500

    Add login helper method to determine if there's an account

--- app/core/login/__init__.py
+++ app/core/login/__init__.py
@@ -4,7 +4,7 @@ from app.core import database
 from app.core.login import password
 
 
-__all__ = ["confirm"]
+__all__ = ["confirm", "has_account"]
 
 
 def confirm(email_addr: str, user_pass: str) -> Optional[bool]:
@@ -16,3 +16,10 @@ def confirm(email_addr: str, user_pass: str) -> Optional[bool]:
 
     # Do the passwords match and the password reset flag is not set?
     return password.confirm(user_pass, user.password) and not user.needs_password_reset
+
+
+def has_account(email_addr: str) -> bool:
+    """Determine if there's an account with the given email address."""
+    user = database.user_get_login(email_addr)
+    return True if user is not None else False
+
