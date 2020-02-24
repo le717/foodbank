60d7d4495f80a9f250753c1a16586e5d2f0214e6
Caleb Ely <le717@users.noreply.github.com>
2020-02-23 22:35:03 -0500

    Internal cleanup

--- app/core/login/__init__.py
+++ app/core/login/__init__.py
@@ -1,18 +1,16 @@
-from typing import Optional
-
 from app.core import database
 from app.core.login import password
 
 
-__all__ = ["confirm", "has_account"]
+__all__ = ["confirm", "has_account", "flag_password_reset"]
 
 
-def confirm(email_addr: str, user_pass: str) -> Optional[bool]:
+def confirm(email_addr: str, user_pass: str) -> bool:
     """Confirm a valid email address/password combination."""
     # Attempt to find this user
     user = database.user_get_login(email_addr)
     if user is None:
-        return None
+        return False
 
     # Do the passwords match and the password reset flag is not set?
     return password.confirm(user_pass, user.password) and not user.needs_password_reset
@@ -21,7 +19,7 @@ def confirm(email_addr: str, user_pass: str) -> Optional[bool]:
 def has_account(email_addr: str) -> bool:
     """Determine if there's an account with the given email address."""
     user = database.user_get_login(email_addr)
-    return True if user is not None else False
+    return bool(user)
 
 
 def flag_password_reset(email_addr: str) -> str:
