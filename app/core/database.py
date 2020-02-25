c23c301a856937ec0762448b355a1300a8357d42
Caleb Ely <le717@users.noreply.github.com>
2020-02-25 11:55:54 -0500

    Handle exceptions for two db methods

--- app/core/database.py
+++ app/core/database.py
@@ -4,11 +4,10 @@ from typing import Optional
 from flask import current_app
 
 import records
+from sqlalchemy import exc
 
-# from sqlalchemy.exc import IntegrityError
 
-
-__all__ = ["user_get_login", "user_flag_password_reset", "user_is_reset_token_valid"]
+__all__ = ["user_get_login", "user_flag_password_reset", "user_is_reset_token_valid", "user_reset_password"]
 
 
 def __connect_to_db() -> records.Database:
@@ -19,8 +18,7 @@ def __connect_to_db() -> records.Database:
         current_app.config["mysql_host"],
         current_app.config["mysql_dbname"],
     )
-    conn = records.Database(conn_str)
-    return conn
+    return records.Database(conn_str)
 
 
 def __get_sql_script(script_name: str) -> str:
@@ -35,18 +33,21 @@ def __get_sql_script(script_name: str) -> str:
 def user_get_login(email_addr: str) -> Optional[records.RecordCollection]:
     """Get basic user information to determine a successful login attempt."""
     sql = __get_sql_script("user_login")
-    # TODO Properly handle error state
-    with __connect_to_db() as db:
-        return db.query(sql, email_addr=email_addr).one()
+    try:
+        with __connect_to_db() as db:
+            return db.query(sql, email_addr=email_addr).one()
+    except exc.SQLAlchemyError:
+        return None
 
 
 def user_flag_password_reset(email_addr: str, token: str) -> bool:
     """Reset a user's password."""
     sql = __get_sql_script("user_flag_password_reset")
-    # TODO Properly determine error state
-    with __connect_to_db() as db:
-        return db.query(sql, temp_password_token=token, email_addr=email_addr)
-    return True
+    try:
+        with __connect_to_db() as db:
+            return db.query(sql, temp_password_token=token, email_addr=email_addr)
+    except exc.SQLAlchemyError:
+        return False
 
 
 def user_is_reset_token_valid(token: str) -> bool:
@@ -59,7 +60,7 @@ def user_is_reset_token_valid(token: str) -> bool:
 def user_reset_password(token: str, user_pass: str) -> bool:
     """TODO write me!"""
     sql = __get_sql_script("user_reset_password")
-
+    # TODO Properly determine error state
     with __connect_to_db() as db:
         db.query(sql, token=token, password=user_pass)
         return True
